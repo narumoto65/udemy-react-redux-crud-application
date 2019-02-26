b794796b03d14b0dedfe0f076a0c13ea1b145dfa
narumoto65 <narumoto65@gmail.com>
2019-02-26 16:36:18 +0900

    redux yay

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux'
+import {increment,decrement} from '../actions'
 class App extends Component {
   render() {
+    const props=this.props
     return (
-      <div>
-        <h1>
-            Hello World!
-        </h1>
-      </div>
+      <React.Fragment>
+        <div>value:{props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({value:state.count.value})
+
+const mapDispatchToProps=({increment,decrement})
+//const mapDispatchToProps = dispatch => ({
+//  increment:()=>dispatch(increment()),
+//  decrement:()=> dispatch(decrement())
+//})
+export default connect(mapStateToProps,mapDispatchToProps)(App)
